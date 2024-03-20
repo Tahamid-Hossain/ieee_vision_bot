0583bd7c5b5c4f34c2137f49041b1e8bfe2a417c
tahamid <mitahamid13@gmail.com>
2024-03-20 04:10:12 +0400

    changes the folder to actual one

--- vision_rpi_bot/setup.py
+++ vision_rpi_bot/setup.py
@@ -11,16 +11,13 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share',package_name,'meshes'),glob('meshes/*')),
         (os.path.join('share',package_name,'launch'),glob('launch/*')),
-        (os.path.join('share',package_name,'urdf'),glob('urdf/*')),
-        (os.path.join('share',package_name,'worlds'),glob('worlds/*')),
-        (os.path.join('share',package_name,'data'),glob('data/*'))
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='th',
-    maintainer_email='example@email.com',
+    maintainer='luqman',
+    maintainer_email='luqman@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
@@ -28,10 +25,10 @@ setup(
         'console_scripts': [
                 'publisher_rpi_node = vision_rpi_bot.publisher:main',
                 'subscriber_rpi_node = vision_rpi_bot.subscriber:main',
-                'qr_maze_solve_node = vision_rpi_bot.qr_maze_drive:main',
-                'line_following_sim_node = vision_rpi_bot.line_following_sim:main',
-                'line_following_real_node = vision_rpi_bot.line_following_real:main',
-                'surveillance_node = vision_rpi_bot.surveillance_bot:main',
+                'cmdVel_to_pwm_node = vision_rpi_bot.cmd_to_pwm_driver:main',
+                'image_publisher = vision_rpi_bot.image_publisher:main',
+                'line_follow_drive_node = vision_rpi_bot.line_follow_drive:main',
+
                            ],
                 },
 )
